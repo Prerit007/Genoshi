a66f49c29f38af7e15102017153663aa2a10bfb0
Prerit007 <75621370+Prerit007@users.noreply.github.com>
2023-12-29 16:54:55 +0530

    Pricing-Page-changes

--- Genoshi/src/components/PriceCard.tsx
+++ Genoshi/src/components/PriceCard.tsx
@@ -1,25 +1,36 @@
 interface PriceCardProps {
   name: string,
+  plan: string,
   description: string,
   binText: string,
   price: string,
   features: string[],
   color: string,
+  selected?: boolean,
+  onSelect: () => void,
 }
 
-const PriceCard = ({name, description, binText, price, features, color}: PriceCardProps) => {
+const PriceCard = ({name, plan,description, binText, price, features, color, selected, onSelect}: PriceCardProps) => {
   return (
-    <div 
-    style={{backgroundColor: color}}
-    className="flex min-h-[428px] w-[320px] flex-col rounded-3xl p-8">
-      <h2 className="mb-5 text-3xl font-bold text-center">{name}</h2>
-      <div className="mb-5 flex items-end text-6xl font-black">
-        {price ? (<> <div>${price}</div></>): ("\n")}
-      </div>
-      <p className="mb-5">{description}</p>
+    <div className="flex flex-col items-center">
+      <input
+        type="radio"
+        name="plan"
+        id={`${plan}-plan`}
+        value={plan}
+        checked={selected}
+        onChange={onSelect}
+      />
+      <label htmlFor={`${plan}-plan`}>{name}</label>
+      <div style={{ backgroundColor: color, opacity: selected ? 1 : 0.5 }} className="flex min-h-[428px] w-[320px] flex-col rounded-3xl p-8 mt-4">
+        <h2 className="mb-5 text-5xl font-bold text-center">{name}</h2>
+        <div className="mb-5 flex items-end text-3xl font-black">
+        {price ? (<> <div>${price}/month</div></>): (" ")}
+        </div>
+        <p className="mb-5">{description}</p>
       <ul className="mb-10 flex flex-col gap-y-2">
         {features.map((feature) => (
-          <li className="flex items-center">
+          <li className="flex items-center gap-x-2">
             <svg fill="#000000" height="16px" width="16px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
 	 viewBox="0 0 191.667 191.667">
 <path d="M95.833,0C42.991,0,0,42.99,0,95.833s42.991,95.834,95.833,95.834s95.833-42.991,95.833-95.834S148.676,0,95.833,0z
@@ -33,7 +44,9 @@ const PriceCard = ({name, description, binText, price, features, color}: PriceCa
         ))}
       </ul>
       <button className="mt-auto rounded-xl bg-black py-3 px-6 text-lg font-medium text-white">{binText}</button>
+      </div>
     </div>
+    
   )
 }
 
--- Genoshi/src/pages/Pricing.tsx
+++ Genoshi/src/pages/Pricing.tsx
@@ -1,7 +1,8 @@
-
+import { useState } from 'react';
 import PriceCard from "../components/PriceCard"
 
 const Pricing = () => {
+  const [selectedPlan, setSelectedPlan] = useState('basic');
   return (
     <div className="flex flex-col items-center bg-black p-4">
       <div className="mb-2 mt-12 text-center">
@@ -13,24 +14,33 @@ const Pricing = () => {
         <PriceCard
         color = "#78e3fc"
         name = "Basic"
+        plan='basic'
         description ="Get started with the basic plan"
         price="4"
-        features ={["1 User", "10 Graphs", "500 Chatbot Queries"]}
-        binText="Start Free Plan"/>
+        features ={["1 User", "Upto 10 Graphs", "Upto 500 Chatbot Queries"]}
+        binText="Start Free Plan"
+        selected={selectedPlan === 'basic'}
+        onSelect={()=> setSelectedPlan("basic")}/>
         <PriceCard
         color = "#fcd638"
         name = "Pro"
+        plan = "pro"
         description ="Get started with more Advanced plan"
         price="8"
-        features ={["1 User", "50 Graphs", "1000 Chatbot Queries", "Email + Live Chat Support"]}
-        binText="Start Pro Plan"/>
+        features ={["1 User", "Upto 50 Graphs", "Upto 1000 Chatbot Queries", "Email + Live Chat Support"]}
+        binText="Start Pro Plan"
+        selected={selectedPlan === 'pro'}
+        onSelect={()=> setSelectedPlan("pro")}/>
         <PriceCard
         color = "#ffb5ba"
         name = "Enterprise"
-        description ="For Enterprises"
+        plan = "enterprise"
+        description =""
         price = ""
         features ={["Unlimited Graphs", "Able to Upload Custom PDFs", "24/7 Phone + Email Support"]}
-        binText="Contact for Pricing"/>
+        binText="Contact for Pricing"
+        selected={selectedPlan === 'enterprise'}
+        onSelect={()=> setSelectedPlan("enterprise")}/>
       </div>
     </div>
   )
